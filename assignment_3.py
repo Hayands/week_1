01f97852a260719236e60aeb3e2ba0aa19faea40
Hayands <quinnpoulin3@gmail.com>
2025-01-26 14:24:56 -0500

    new file added

--- assignment_3.py
+++ assignment_3.py
@@ -13,7 +13,14 @@ Year 1: 2025
 Year 2: 2028
 Difference: 3
 '''
+# Input the years
+Year1 = int(input("Enter a year please:"))
+Year2 = int(input("Enter another year please:"))
 
+# Output the years and the differance
+print("Year 1:", Year1)
+print("Year 2:", Year2)
+print("Differance:", abs(Year1 - Year2))
 
 #%%
 # Second Assignment
@@ -27,7 +34,15 @@ Your output should look like:
 Fahrenheit: 25
 Celsius: -3.89
 '''
+# Input temperature in Fahrenheit
+fahrenheit = float(input("Please provide a temperature in Farenheit:"))
 
+# Convert Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5.0 / 9.0
+
+# Display Fahrenheit and Celsius temperatures
+print("Fahrenheit:", fahrenheit)
+print("Celsius:", round(celsius, 2))
 
 #%%
 # Third Assignment
@@ -42,6 +57,20 @@ USD: 1
 EU: 0.97
 
 '''
+# Input USD amount
+usd = float(input("Enter a USD amount:"))
+
+# Define the exchange rate
+exchange_rate = 0.97
+
+# Convert from USD to Euros
+euros = usd * exchange_rate
+
+# Display the USD and Euros
+print("USD:", usd)
+print("EU:", round(euros, 2))
+
+
 
 
 ##### ASSIGNMENT ENDS HERE #####
